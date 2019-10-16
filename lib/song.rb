2b50cc21194d80daf84e9797db76cc4c8d919404
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-16 15:23:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,11 @@ class Song
     
     def self.new_from_filename(name)
       song = self.new
-      
+      song.name = (name.split(" - ")[1].chomp(".mp3"))
+      song.artist_name = (name.split(" - ")[0]
+      @@all << song
+      song
+    end
 end
 
 
